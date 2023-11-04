d97b0dfbae432ee64bee4f9e34de992e4ccb33d3
Edison Augusthy <edison>
2023-11-04 18:32:04 +0530

    angular upgrade

--- projects/drag-me/src/public-api.ts
+++ projects/drag-me/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of drag-me
  */
 
-export * from './lib/drag-me.service';
 export * from './lib/Directive/dragger.directive';
 export * from './lib/Directive/dropper.directive';
 export * from './lib/drag-me.module';
