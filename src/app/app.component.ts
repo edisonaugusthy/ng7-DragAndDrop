c2b45fdd9b792e3e611a0ff310d8a44920d6458e
Edison Augusthy <edison>
2023-11-05 14:16:37 +0530

    updated sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = [...Array(10).keys()].map(n => n + 1)
-  details = [...Array(10).keys()].map(n => n + 10)
+  array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  array2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-  onDrop(e) {
-    console.log(`drop 2`)
-  }
   tDrop(e) {
-    console.log(`drop 1`)
+    this.array1 = this.array1.filter((item) => {
+      return item !== e;
+    });
+  }
+  onDrop(e) {
+    this.array2.push(e);
+    this.array2.sort((a, b) => a - b);
   }
 }
