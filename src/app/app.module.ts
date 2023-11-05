d7c337f768a559bc5aea4fb5e0438766f185f7d2
Edison Augusthy <edison>
2023-11-05 12:59:37 +0530

    updated readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DragMeModule } from 'drag-me';
 
 import { AppComponent } from './app.component';
-import { DragMeModule } from 'drag-me';
 
 @NgModule({
   declarations: [
